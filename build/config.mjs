35f7a7cadbc6754141a2a85c63c88f317632efeb
Irina Sklyarova <irina060981@yandex.ru>
2018-08-15 21:50:05 +1000

    fix package.json

--- build/config.mjs
+++ build/config.mjs
@@ -13,9 +13,26 @@ const webpack = {
     mode: "development",
     node: {
       __dirname: false
-    },
+    }
+  },
+  production: {
     resolve: {
-      modules: ['node_modules']
+      alias: {
+        'alpheios-data-models': path.join(projectRoot, 'node_modules/alpheios-data-models/dist/alpheios-data-models.node.min.js'),
+        'alpheios-morph-client': path.join(projectRoot, 'node_modules/alpheios-morph-client/dist/alpheios-morph-client.node.min.js'),
+        'alpheios-lexicon-client': path.join(projectRoot, 'node_modules/alpheios-lexicon-client/dist/alpheios-lexicon-client.node.min.js'),
+        'alpheios-lemma-client': path.join(projectRoot, 'node_modules/alpheios-lemma-client/dist/alpheios-lemma-client.node.min.js')
+      }
+    }
+  },
+  development: {
+    resolve: {
+      alias: {
+        'alpheios-data-models': path.join(projectRoot, 'node_modules/alpheios-data-models/dist/alpheios-data-models.node.js'),
+        'alpheios-morph-client': path.join(projectRoot, 'node_modules/alpheios-morph-client/dist/alpheios-morph-client.node.js'),
+        'alpheios-lexicon-client': path.join(projectRoot, 'node_modules/alpheios-lexicon-client/dist/alpheios-lexicon-client.node.js'),
+        'alpheios-lemma-client': path.join(projectRoot, 'node_modules/alpheios-lemma-client/dist/alpheios-lemma-client.node.js')
+      }
     }
   }
 }
